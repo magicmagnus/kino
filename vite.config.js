c5477f64f3a5532c5354fa53e9bf866147fc1b5d
Magnus Kaut <67912067+magicmagnus@users.noreply.github.com>
2025-06-03 22:57:57 +0200

    redirect fix 4

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
         host: true, // Allows the server to be accessed over the network
         port: 3000, // Optional: Specify a custom port (default is 5173)
     },
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, "index.html"),
+                404: path.resolve(__dirname, "public/404.html"),
+            },
+        },
+    },
 });
