4fea6fc0290f6b954d65b789bde9239d8fcff4e2
Magnus Kaut <67912067+magicmagnus@users.noreply.github.com>
2025-06-04 22:56:40 +0200

    include generateSitemap in scraper

--- scripts/generateSitemap.mjs
+++ scripts/generateSitemap.mjs
@@ -3,24 +3,46 @@ import path from "path";
 
 const baseUrl = "https://kinoschurke.de"; // Replace with your actual website URL
 const moviesFilePath = path.join("src", "data", "movies-reference.json");
+const eventsFilePath = path.join("src", "data", "event-view.json");
+const roomsFilePath = path.join("src", "data", "room-view.json");
+const datesFilePath = path.join("src", "data", "date-view.json");
 
 async function generateSitemap() {
     const moviesData = JSON.parse(await fs.readFile(moviesFilePath, "utf8"));
-    const moviePages = Object.values(moviesData).map(
-        (movie) => `/movie/${movie.slug}`,
-    );
+    const eventsData = JSON.parse(await fs.readFile(eventsFilePath, "utf8"));
+    const roomsData = JSON.parse(await fs.readFile(roomsFilePath, "utf8"));
+    const datesData = JSON.parse(await fs.readFile(datesFilePath, "utf8"));
 
-    const staticPages = [
-        "/",
-        "/date",
-        "/room",
-        "/movie",
-        // Add more static pages here
+    const moviePages = Object.values(moviesData).map(
+        (movie) => `/movies/${movie.slug}`,
+    );
+    const eventPages = eventsData.map((event) => `/events/${event.slug}`);
+    const roomPages = roomsData
+        .map((theater) => theater.rooms.map((room) => `/rooms/${room.slug}`))
+        .flat();
+    const datePages = datesData.map((date) => `/dates/${date.date}`);
+
+    const staticPages = ["/", "/dates", "/rooms", "/movies", "/events"];
+
+    const dynamicPages = [
+        ...moviePages,
+        ...eventPages,
+        ...roomPages,
+        ...datePages,
     ];
 
-    const pages = [...staticPages, ...moviePages];
+    const staticUrls = staticPages
+        .map(
+            (page) => `
+    <url>
+        <loc>${baseUrl}${page}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+    </url>`,
+        )
+        .join("");
 
-    const urls = pages
+    const dynamicUrls = dynamicPages
         .map(
             (page) => `
     <url>
@@ -34,7 +56,8 @@ async function generateSitemap() {
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
+    ${staticUrls}
+    ${dynamicUrls}
   </urlset>`;
 
     const sitemapPath = path.join("public", "sitemap.xml");
