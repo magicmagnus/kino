dabb21aa08c0b8e18601cc3261fe3cd2b8ada6a3
Magnus Kaut <67912067+magicmagnus@users.noreply.github.com>
2025-06-04 19:38:19 +0200

    [move to netlify]

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,12 +8,5 @@ export default defineConfig({
         host: true, // Allows the server to be accessed over the network
         port: 3000, // Optional: Specify a custom port (default is 5173)
     },
-    build: {
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, "index.html"),
-                404: path.resolve(__dirname, "public/404.html"),
-            },
-        },
-    },
+    base: "/", // Should already be correct for custom domain
 });
