1e646b425754f7eca89f5022f7e4a161615bc4ab
Magnus Kaut <67912067+magicmagnus@users.noreply.github.com>
2025-08-22 18:08:27 +0200

    reset head to working before?

--- src/components/SEOHead.jsx
+++ src/components/SEOHead.jsx
@@ -6,7 +6,6 @@ const SEOHead = ({
     image = "https://kinoschurke.de/preview_image.png",
     url,
     movieTitle,
-    movieSlug,
     eventName,
     roomName,
     date,
@@ -14,28 +13,14 @@ const SEOHead = ({
     // Generate dynamic content based on props
     let pageTitle = title;
     let pageDescription = description;
-    let pageUrl =
-        url || (typeof window !== "undefined" ? window.location.href : "");
-    let primaryImage = image;
-    let twitterImage = "https://kinoschurke.de/preview_image_sq.png";
+    let pageUrl = url || window.location.href;
 
-    if (movieTitle && movieSlug) {
+    if (movieTitle) {
         pageTitle = `${movieTitle} - Kinoschurke`;
-        pageDescription = `Alle Vorstellungen von "${movieTitle}" in Tübingen.`;
-        // Use the same domain as your primary domain (no www)
-        primaryImage = `https://kinoschurke.de/poster-variants/og/${movieSlug}.png`;
-        twitterImage = `https://kinoschurke.de/poster-variants/square/${movieSlug}.png`;
-
-        // Debug logging
-        console.log("SEO Debug:", {
-            movieTitle,
-            movieSlug,
-            primaryImage,
-            twitterImage,
-        });
+        pageDescription = `Alle Vorstellungen von "${movieTitle}" in Tübinger Kinos.`;
     } else if (eventName) {
         pageTitle = `${eventName} - Kinoschurke`;
-        pageDescription = `Alle "${eventName}" Vorstellungen in Tübingen.`;
+        pageDescription = `Alle "${eventName}" Vorstellungen in Tübinger Kinos.`;
     } else if (roomName) {
         pageTitle = `${roomName} - Kinoschurke`;
         pageDescription = `Das Kinoprogramm für ${roomName} in Tübingen.`;
@@ -54,20 +39,9 @@ const SEOHead = ({
             <meta property="og:type" content="website" />
             <meta property="og:title" content={pageTitle} />
             <meta property="og:description" content={pageDescription} />
-
-            {/* Single primary OG image */}
-            <meta property="og:image" content={primaryImage} />
-            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image" content={image} />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
-            <meta
-                property="og:image:alt"
-                content={
-                    movieTitle
-                        ? `${movieTitle} Poster`
-                        : "Kinoschurke - Tübinger Kinoprogramm"
-                }
-            />
 
             {/* Twitter */}
             <meta name="twitter:card" content="summary_large_image" />
@@ -75,15 +49,15 @@ const SEOHead = ({
             <meta property="twitter:url" content={pageUrl} />
             <meta name="twitter:title" content={pageTitle} />
             <meta name="twitter:description" content={pageDescription} />
-            <meta name="twitter:image" content={twitterImage} />
+            <meta name="twitter:image" content={image} />
+
+            {/* WhatsApp square image */}
             <meta
-                name="twitter:image:alt"
-                content={
-                    movieTitle
-                        ? `${movieTitle} Poster`
-                        : "Kinoschurke - Tübinger Kinoprogramm"
-                }
+                property="og:image"
+                content="https://kinoschurke.de/preview_image_sq.png"
             />
+            <meta property="og:image:width" content="600" />
+            <meta property="og:image:height" content="600" />
         </Helmet>
     );
 };
